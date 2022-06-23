5c600727e5219f435a84af21f7eb3853969abc45
saxon <1@1.com>
2022-06-23 20:34:41 +0430

    withdraw token fucntion

--- contracts/Wallet.sol
+++ contracts/Wallet.sol
@@ -21,9 +21,38 @@ contract Wallet  is Compound {
         IERC20(underlyingAddress).transferFrom(msg.sender, address(this), underlyingAmount);
         supply(cTokenAddress, underlyingAmount);
     }
+
+    function withdraw(
+        address cTokenAddress,
+        uint underlyingAmount,
+        address recipient
+    ) 
+    onlyAdmin()
+    external{
+        require(
+            getUnderlyingBalance(cTokenAddress) >= underlyingAmount,
+            'balance too low'
+        );
+        claimComp();
+        redeem(cTokenAddress, underlyingAmount);
+
+        address underlyingAddress = getUnderlyingAddress(cTokenAddress);
+        IERC20(underlyingAddress).transfer(recipient, underlyingAmount);
+
+        address compAddress = getCompAddress();
+        IERC20 compToken = IERC20(compAddress);
+        uint compAmount = compToken.balanceOf(address(this));
+        compToken.transfer(recipient, compAmount);
+    }
     
     receive() external payable {
         supplyEth(msg.value);
     }
+
+    modifier onlyAdmin(){
+        require(msg.sender == admin, 'only Admin');
+        _;
+    }
+    
 
 }

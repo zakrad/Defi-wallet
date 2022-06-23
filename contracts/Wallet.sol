e4c9af40860ac8ac76e1051092540c8603bd6d91
saxon <1@1.com>
2022-06-23 20:07:33 +0430

    deposit token

---
+++ contracts/Wallet.sol
@@ -0,0 +1,24 @@
+pragma solidity ^0.7.3;
+
+import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+import'./Compound.sol';
+
+contract Wallet  is Compound {
+    address public  admin;
+
+    constructor (
+        address _comptroller,
+        address _cEthAddress
+    ) Compound (_comptroller, _cEthAddress) {
+        admin = msg.sender;
+    }
+
+    function deposit(
+        address cTokenAddress,
+        uint underlyingAmount
+    ) external {
+        address underlyingAddress = getUnderlyingAddress(cTokenAddress);
+        IERC20(underlyingAddress).transferFrom(msg.sender, address(this), underlyingAmount);
+        supply(cTokenAddress, underlyingAmount);
+    }
+}
